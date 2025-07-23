608ce7760cd463d7498fb42e9d366c35a8d04b13
khizar11448 <khizaryounas777@gmail.com>
2025-07-24 02:04:34 +0500

    do coursea project

---
+++ myapp/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import Nav from './components/Nav';
+import Promo from './components/Promo';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Footer from './components/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Nav />
+    <br />
+    <Promo />
+    <br />
+    
+    <Intro1 />
+    <br />
+    
+     <Intro2 />
+     <br />
+     
+      <Intro3 />
+      <br />
+      
+      <Footer />
+      </div>
+    
+  );
+}
+
+export default App;
